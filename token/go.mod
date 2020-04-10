7cbb7998e1b84246d25a83269590f3d7e8bec448
冨谷勢一朗 <stirot@stirots.local>
2020-04-10 22:39:40 +0900

    firest commit

---
+++ token/go.mod
@@ -0,0 +1,3 @@
+module local.com/monkey/token
+
+go 1.13
---
+++ token/token.go
@@ -0,0 +1,35 @@
+// token/token.go
+package monkey/token
+
+type  TokenType string
+
+type Token struct {
+  Type    TokenType
+  Literal string
+}
+
+const (
+  ILLEGAL = "ILLEGAL"
+  EOF = "EOF"
+
+  // 識別子＋リテラル
+  IDENT = "IDENT" // add, foobar, x, y, ...
+  INT = "INT" //123456
+
+  // 識別子
+  ASSIGN = "="
+  PLUS = "+"
+
+  // デリメタ
+  COMMA = ","
+  SEMICOLON = ";"
+
+  LPAREN = "("
+  RPANEN = ")"
+  LBRACE = "{"
+  RBRACE = "}"
+
+  // キーワード
+  FUNCTION = "FUNCTION"
+  LET = "LET"
+)
