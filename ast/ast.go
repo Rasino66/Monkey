d8264a9d9543bed7d7b3f42d4f70fd9c73ee2e8a
冨谷勢一朗 <stirot@stirots.local>
2020-04-19 05:45:47 +0900

    add ast/ast.go

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+//ast/ast.go
+
+package ast
+
+import (
+	"github.com/Rasino66/Monkey/token"
+)
+
+type Node interface {
+  TokenLiteral() string
+}
+
+type Statement interface {
+  Node
+  statementNode()
+}
+
+type Expression interface {
+  Node
+  expressionNode()
+}
+
+type Program struct {
+  Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string{
+  if len(p.Statement) > 0 {
+    return p.Statement[0].TokenLiteral()
+  } else {
+    return ""
+  }
+}
+
+type LetStatement struct {
+  Token token.Token // token.LET
+  Name *Identifier
+  Value Expression
+}
+
+func (ls *LetStatement) StatementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal}
+
+type Identifier struct {
+  Token token.Token // token.IDENT
+  value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+
+
