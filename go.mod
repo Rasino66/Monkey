9530dcfbfaad985a33aab9a5c48af62276fb0026
Rasino <a.rasino66@gmail.com>
2020-07-12 21:38:49 +0900

    3.6 fin

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/Rasino66/Monkey
 
 go 1.13
+
+require (
+	github.com/google/pprof v0.0.0-20200708004538-1a94d8640e99 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20200705045648-b855e248e31b // indirect
+	github.com/tkancf/monkey v0.0.0-20200624032034-c14931d77c62 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+)
--- main.go
+++ main.go
@@ -2,20 +2,27 @@
 
 package main
 
-
 import (
-  "fmt"
-  "os"
-  "os/user"
+	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/user"
+
 	"github.com/Rasino66/Monkey/repl"
 )
 
 func main() {
-  user, err := user.Current()
-  if err != nil {
-    panic(err)
-  }
-  fmt.Printf("Hello %s! This is the Monkey programming language!\n",user.Username)
-  fmt.Printf("Feel free to tyoe in commands\n")
-  repl.Start(os.Stdin, os.Stdout)
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to tyoe in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
